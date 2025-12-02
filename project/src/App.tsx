859e9c975f9da5fab3e946ae7b93ae921ded3614
agustfricke <hej@agustfricke.com>
2025-12-02 13:44:39 +0100

    commit from n73.ai

---
+++ project/src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="min-h-screen bg-black flex items-center justify-center">
+      <h1 className="text-white text-4xl">Hello World</h1>
+    </div>
+  );
+}
+
+export default App;
